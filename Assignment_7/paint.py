f48c390329112edc4b388a598bc911a47544f974
Rajkumar Sawant <58840962+codeplusmath@users.noreply.github.com>
2020-12-09 14:08:47 +0530

    Update paint.py

--- Assignment_7/paint.py
+++ Assignment_7/paint.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.colorchooser import askcolor
+from tkinter.filedialog import asksaveasfile
 
 
 class Paint(object):
@@ -9,24 +10,25 @@ class Paint(object):
 
     def __init__(self):
         self.root = Tk()
+        self.root.title("My Paint")
 
         self.pen_button = Button(self.root, text='pen', command=self.use_pen)
-        self.pen_button.grid(row=0, column=0)
+        self.pen_button.grid(row=0, column=1)
 
         self.brush_button = Button(self.root, text='brush', command=self.use_brush)
-        self.brush_button.grid(row=0, column=1)
+        self.brush_button.grid(row=0, column=2)
 
         self.color_button = Button(self.root, text='color', command=self.choose_color)
-        self.color_button.grid(row=0, column=2)
+        self.color_button.grid(row=0, column=3)
 
         self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
-        self.eraser_button.grid(row=0, column=3)
+        self.eraser_button.grid(row=0, column=4)
 
-        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
-        self.choose_size_button.grid(row=0, column=4)
+        self.choose_size_button = Scale(self.root, from_=1, to=50, orient=HORIZONTAL)
+        self.choose_size_button.grid(row=0, column=5)
 
-        self.c = Canvas(self.root, bg='white', width=600, height=600)
-        self.c.grid(row=1, columnspan=5)
+        self.c = Canvas(self.root, bg='white', width=1000, height=800)
+        self.c.grid(row=1, columnspan=7)
 
         self.setup()
         self.root.mainloop()
@@ -72,7 +74,9 @@ class Paint(object):
 
     def reset(self, event):
         self.old_x, self.old_y = None, None
-
+    
+    
 
 if __name__ == '__main__':
     Paint()
+
